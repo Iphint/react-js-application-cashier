57d53b25e70b6f92a56363fd753b208029070fa3
Muhammad Arifin <ma7804058@gmail.com>
2022-11-08 17:17:50 +0700

    ubah

--- cashier application/server.js
+++ cashier application/server.js
@@ -1,12 +1,15 @@
-const data = require('../backend/db.json')
-
-const jsonServer = require('json-server')
+const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router(data);
-const middleware = jsonServer.defaults();
-const port = process.env.PORT || 3000;
-
-server.use(middleware);
+const router = jsonServer.router('../backend/db.json');
+const middlewares = jsonServer.defaults({
+  static: './build'
+});
+const PORT = process.env.PORT || 8000;
+server.use(middlewares);
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+}))
 server.use(router);
-
-server.listen(port)
+server.listen(PORT, () => {
+  console.log('Server is running');
+});
